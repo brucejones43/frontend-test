78184708c5cc1ab4b4ecb121ba591f21d1d36ab8
Ralphomines <raphael.kiarie@yahoo.com>
2022-10-04 17:06:03 -0600

    Updated Navbar and some dark mode

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -26,6 +26,7 @@ export class LoginComponent implements OnInit {
       this.authService.login(this.loginForm.get('email')?.value, this.loginForm.get('password')?.value).subscribe(
       () => {
         this.authService.loggedIn=true;
+        sessionStorage.setItem("loggedIn", "true");
       },
       (err) => console.log(err),
       () => this.router.navigate(['home'])
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -20,6 +20,42 @@ export class NavbarComponent implements OnInit{
     this.subscription = this.productService.getCart().subscribe(
       (cart) => this.cartCount = cart.cartCount
     );
+
+    console.log(localStorage.getItem("mode"));
+
+    if (localStorage.getItem("mode") === "dark") {
+      
+      let darkModeToggler = document.getElementById("checkbox");
+      darkModeToggler?.setAttribute("checked", "true");
+
+      let bodyBackground = document.getElementsByClassName("body-light-background");
+      bodyBackground[0].classList.add('body-dark-background');
+    
+      let lightForeground = document.getElementsByClassName("light-foreground");
+      let lightBackground = document.getElementsByClassName("light-background");
+      let lightBackground2 = document.getElementsByClassName("light-background-2");
+    
+      //var darkForeground = document.getElementsByClassName("dark-foreground");
+      var darkBackground = document.getElementsByClassName("dark-background");
+    
+      for (let i = 0; i < lightBackground.length; i++) {
+        
+        //lightBackground[i].classList.toggle('light-background');
+        lightBackground[i].classList.add('dark-background');
+        //lightBackground[i].classList.replace("light-background", "dark-background");
+      }
+    
+      for (let i = 0; i < lightBackground2.length; i++) {
+        //lightBackground[i].classList.toggle('light-background');
+        lightBackground2[i].classList.add('dark-background-2');
+        //lightBackground[i].classList.replace("light-background", "dark-background");
+      }
+    
+      for (let i = 0; i < lightForeground.length; i++) {
+        lightForeground[i].classList.add("dark-foreground");
+      }
+    }
+
   }
 
   ngOnDestroy() {
@@ -28,7 +64,53 @@ export class NavbarComponent implements OnInit{
 
   logout() {
     this.authService.logout();
+    sessionStorage.setItem("loggedIn", "false");
     this.router.navigate(['login']);
+  }
+  
+  get isLoggedIn(){
+    if (sessionStorage.getItem("loggedIn") === "true") {
+      return true;
+    } else {
+      return false;
+    }
+    
+  }
+
+  toggleDarkMode() {
+    console.log("TOGGLE");
+    
+    
+    if (localStorage.getItem("mode") === "dark") {
+      localStorage.removeItem("mode");
+    } else {
+      localStorage.setItem("mode", "dark");
+    }
+    let bodyBackground = document.getElementsByClassName("body-light-background");
+    bodyBackground[0].classList.toggle('body-dark-background');
+
+    let lightForeground = document.getElementsByClassName("light-foreground");
+    let lightBackground = document.getElementsByClassName("light-background");
+    let lightBackground2 = document.getElementsByClassName("light-background-2");
+
+    //var darkForeground = document.getElementsByClassName("dark-foreground");
+    var darkBackground = document.getElementsByClassName("dark-background");
+
+    for (let i = 0; i < lightBackground.length; i++) {
+      //lightBackground[i].classList.toggle('light-background');
+      lightBackground[i].classList.toggle('dark-background');
+      //lightBackground[i].classList.replace("light-background", "dark-background");
+    }
+
+    for (let i = 0; i < lightBackground2.length; i++) {
+      //lightBackground[i].classList.toggle('light-background');
+      lightBackground2[i].classList.toggle('dark-background-2');
+      //lightBackground[i].classList.replace("light-background", "dark-background");
+    }
+
+    for (let i = 0; i < lightForeground.length; i++) {
+      lightForeground[i].classList.toggle("dark-foreground");
+    }
   }
 
 }
