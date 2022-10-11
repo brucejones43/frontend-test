b021e25697d313d649eeb7951cfd0d767221362f
ssebast31 <ssebastianneri@gmail.com>
2022-10-11 11:20:09 -0700

    added testing for about page

--- src/app/components/about/about.component.spec.ts
+++ src/app/components/about/about.component.spec.ts
@@ -22,4 +22,21 @@ describe('AboutComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`images has default value`, () => {
+    expect(component.images).toEqual([]);
+  });
+
+  it(`selectedIndex has default value`, () => {
+    expect(component.selectedIndex).toEqual(0);
+  });
+
+  describe('ngOnInit', () => {
+    it('makes expected calls', () => {
+      spyOn(component, 'autoSlideImages').and.callThrough();
+      component.ngOnInit();
+      expect(component.autoSlideImages).toHaveBeenCalled();
+    });
+  });
+  
 });
