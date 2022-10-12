d5a87198cce3f31473ae0660948c60c81376fd21
brucejones43 <sundberg43@gmail.com>
2022-10-12 11:08:33 -0700

    quickfix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,12 +4,6 @@ export const environment = {
   baseUrl: "http://ec2-34-212-106-51.us-west-2.compute.amazonaws.com:8080",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://soulsounds.s3-website-us-west-2.amazonaws.com/',
-  },,
-  withCredentials: true,
-  baseUrl: "http://ec2-34-212-106-51.us-west-2.compute.amazonaws.com:8080",
-  headers: {
-    'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://ec2-34-212-106-51.us-west-2.compute.amazonaws.com:8080',
   },
 };
